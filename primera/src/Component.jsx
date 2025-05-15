76a0f55078e52edbaebe7793da0241d9911587ac
MoisesN <moisesmendez336@.com>
2025-05-15 15:42:58 +0200

    agregando mi primer react

---
+++ primera/src/Component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function PrimerDiv(){
+
+  return (
+    <div>
+        <p>mi primer component</p>
+        
+    <input type="text" value="" />
+    <button type="button">Actualiar</button>
+    </div>
+  )
+
+
+}
